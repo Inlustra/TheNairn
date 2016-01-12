e496dbe0c420a0e96098c00da17467d13f5110f6
Thomas Nairn <thomas.nairn@thinkinnovate.co.uk>
2016-01-12 15:53:42 +0000

    Fixed up the build script

--- src/js/RGB.js
+++ src/js/RGB.js
@@ -11,16 +11,16 @@ function RGB(r, g, b) {
 }
 
 RGB.prototype.toString = function () {
-    return "rgb(" + this.r + ", " + this.g + ", " + this.b + ")";
+    return 'rgb(' + this.r + ', ' + this.g + ', ' + this.b + ')';
 };
 
 RGB.prototype.toHex = function () {
     function componentToHex(c) {
         var hex = c.toString(16);
-        return hex.length == 1 ? "0" + hex : hex;
+        return hex.length == 1 ? '0' + hex : hex;
     }
 
-    return "#" + componentToHex(this.r) + componentToHex(this.g) + componentToHex(this.b);
+    return '#' + componentToHex(this.r) + componentToHex(this.g) + componentToHex(this.b);
 };
 
 RGB.prototype.toHSL = function () {
@@ -33,15 +33,27 @@ RGB.prototype.toHSL = function () {
         return (a > b) ? ((a > c) ? a : c) : ((b > c) ? b : c);
     }
 
-    var h, s, l;
+    var h;
+    var s;
+    var l;
     var max = max3(this.r, this.g, this.b);
     var dif = max - min3(this.r, this.g, this.b);
     s = (max == 0.0) ? 0 : (100 * dif / max);
-    if (s == 0) h = 0;
-    else if (this.r == max) h = 60.0 * (this.g - this.b) / dif;
-    else if (this.g == max) h = 120.0 + 60.0 * (this.b - this.r) / dif;
-    else if (this.b == max) h = 240.0 + 60.0 * (this.r - this.g) / dif;
-    if (h < 0.0) h += 360.0;
+    if (s == 0) {
+        h = 0;
+    }
+    else if (this.r == max) {
+        h = 60.0 * (this.g - this.b) / dif;
+    }
+    else if (this.g == max) {
+        h = 120.0 + 60.0 * (this.b - this.r) / dif;
+    }
+    else if (this.b == max) {
+        h = 240.0 + 60.0 * (this.r - this.g) / dif;
+    }
+    if (h < 0.0) {
+        h += 360.0;
+    }
     l = Math.round(max * 100 / 255);
     h = Math.round(h);
     s = Math.round(s);
@@ -52,7 +64,7 @@ RGB.prototype.complement = function () {
     return this.toHSL().complement().toRGB();
 };
 
-RGB.prototype.opposite = function() {
+RGB.prototype.opposite = function () {
     return this.toHSL().opposite().toRGB();
 };
 
